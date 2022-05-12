4f4320d3fab2d9b3fb0f973a5b4fe5e43764bceb
Jonathan Niemann <jonathan@Jonathans-MacBook-Air.local>
2022-05-11 22:13:17 -0300

    Ordering code - SweetAlert

--- src/components/CheckoutLayout/index.php
+++ src/components/CheckoutLayout/index.php
@@ -131,7 +131,6 @@ $page = json_decode($file);
   <div id="backdrop"></div>
   <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js"></script>
   <script src="/dist/scripts.js?cb=<?php echo date('h:i:s'); ?>"></script>
-  <script src="../assets/scripts/plugins/sweetalert.min.js"></script>
 </body>
 
 </html>
