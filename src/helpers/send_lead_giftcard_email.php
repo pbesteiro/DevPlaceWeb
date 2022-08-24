945d1adf2272d66fce96893a899b67ac870514e1
Jonathan Niemann <jonathan@Jonathans-MacBook-Air.local>
2022-08-24 14:31:17 -0300

    Emails changed

--- src/helpers/send_lead_giftcard_email.php
+++ src/helpers/send_lead_giftcard_email.php
@@ -34,17 +34,17 @@ try {
   $mail->Host = 'relay-hosting.secureserver.net';
   $mail->SMTPAuth = false;
   $mail->Port = 25;
-  $mail->Username = 'info@devplace.tech'; // YOUR gmail email
-  $mail->Password = '1nf0D3vPl4ce';
+  $mail->Username = 'organizacion@devplace.tech'; // YOUR gmail email
+  $mail->Password = 'koygkucktsrcsqzu';
   $mail->ssl = false;
   $mail->SMTPAutoTLS = false;
   $mail->SMTPSecure = false; // Enable TLS encryption, `ssl` also accepted
   $mail->isHTML(true);
 
   // Sender and recipient settings
-  $mail->setFrom('info@devplace.tech', 'DevPlace');
+  $mail->setFrom($emailReceptor, 'DevPlace');
   $mail->addAddress($emailReceptor, 'DevPlace');
-  $mail->addReplyTo('info@devplace.tech', $firstname); // to set the reply to
+  $mail->addReplyTo($emailReceptor, $firstname); // to set the reply to
 
   // Setting the email content
   $mail->IsHTML(true);
